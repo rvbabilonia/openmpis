943baad97bf7ef97331252df33f0e634255ddc2f
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-02-04 12:43:15 +0000

    Fixed password retrieval capability. Added validator.

--- src/com/googlecode/openmpis/action/PasswordAction.java
+++ src/com/googlecode/openmpis/action/PasswordAction.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.googlecode.openmpis.action;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,32 +25,31 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionForward;
 
+import com.googlecode.openmpis.dto.Log;
+import com.googlecode.openmpis.dto.User;
 import com.googlecode.openmpis.form.PasswordForm;
-import com.googlecode.openmpis.model.User;
-import com.googlecode.openmpis.model.SqlMapConfig;
-import com.googlecode.openmpis.model.Log;
-import com.googlecode.openmpis.model.Mail;
+import com.googlecode.openmpis.persistence.ibatis.dao.impl.LogDAOImpl;
+import com.googlecode.openmpis.persistence.ibatis.dao.impl.UserDAOImpl;
+import com.googlecode.openmpis.persistence.ibatis.service.LogService;
+import com.googlecode.openmpis.persistence.ibatis.service.UserService;
+import com.googlecode.openmpis.persistence.ibatis.service.impl.LogServiceImpl;
+import com.googlecode.openmpis.persistence.ibatis.service.impl.UserServiceImpl;
+import com.googlecode.openmpis.util.Configuration;
+import com.googlecode.openmpis.util.Constants;
+import com.googlecode.openmpis.util.Mail;
 
-import com.ibatis.sqlmap.client.SqlMapClient;
-
+import com.googlecode.openmpis.util.Validator;
 import java.sql.Date;
-import java.sql.SQLException;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 
 /**
- * The SearchAction class provides the method to pass queries and results.
+ * The PasswordAction class provides the method to reset a user's password.
  * 
  * @author  <a href="mailto:rvbabilonia@gmail.com">Rey Vincent Babilonia</a>
  */
 public class PasswordAction extends Action {
-    /**
-     * The forwarding instances
-     */
-    private final static String SUCCESS = "success";
-    private final static String FAILURE = "failure";
-    
+
     /**
      * This is the action called from the Struts framework.
      * 
@@ -60,70 +58,125 @@ public class PasswordAction extends Action {
      * @param   request     the HTTP Request we are processing
      * @param   response    the HTTP Response we are processing
      * @return              the forwarding instance
+     * @throws java.lang.Exception
      */
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws SQLException {
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        UserService userService = new UserServiceImpl(new UserDAOImpl());
+        LogService logService = new LogServiceImpl(new LogDAOImpl());
         PasswordForm passwordForm = (PasswordForm) form;
-        User user = null;
-        SqlMapClient sqlMap = SqlMapConfig.getSqlMapInstance();
-        
-        try {
-            sqlMap.startTransaction();
-            user = (User) sqlMap.queryForObject("getUser", passwordForm.getUsername());
-            sqlMap.commitTransaction();
-            sqlMap.endTransaction();
-            
+
+        // Check if form is valid
+        if (isValidAccount(request, form)) {
+            // Retrieve user
+            User user = (User) userService.getUserByUsername(passwordForm.getUsername());
+            String forward = "";
+
+            // Check if user exists
             if (user == null) {
-                request.setAttribute("error", "1");
-                return mapping.findForward(FAILURE);
+                ActionMessages errors = new ActionMessages();
+                errors.add("user", new ActionMessage("error.login.invalid"));
+                saveErrors(request, errors);
+
+                return mapping.findForward(Constants.PASSWORD_REDO);
             } else {
-                if ((passwordForm.getQuestion() == user.getQuestion())
-                        && (passwordForm.getAnswer().equals(user.getAnswer()))) {
+                // Check if security question and answer match
+                if ((passwordForm.getQuestion() == user.getQuestion()) && (passwordForm.getAnswer().equals(user.getAnswer()))) {
+                    // Return email address
                     request.setAttribute("email", user.getEmail());
 
                     // Reset password
                     String password = "op3nmp!s";
                     user.setPassword(password);
 
-                    // Log password modification event
+                    // Set password modification event
                     Log log = new Log();
                     log.setLog("User " + user.getUsername() + " reset his password.");
                     log.setDate(new Date(System.currentTimeMillis()));
-                    
-                    sqlMap.startTransaction();
-                    sqlMap.update("updatePassword", user);
-                    sqlMap.insert("insertLog", log);
-                    sqlMap.commitTransaction();
-                    sqlMap.endTransaction();
 
-                    Mail mail = new Mail();
-                    mail.send("Administrator", "", passwordForm.getAdminEmail(), user.getEmail(),
-                            "Password Retrieval",
-                            "Dear " + user.getFirstName() + "," +
-                            "\n\nYour new password is " + password + ". You received this email because" +
-                            "you have forgotten your password or someone pretending to be you" +
-                            "is trying to log into the system." +
-                            "\n\nYours truly," +
-                            "\nAdministrator",
-                            request.getRemoteAddr());
+                    // Update user password and insert log
+                    userService.updatePassword(user);
+                    logService.insertLog(log);
 
-                    return mapping.findForward(SUCCESS);
+                    // Retrieve mail properties
+                    Configuration config = new Configuration("mail.properties");
+                    // Check if email sending is enabled
+                    if (Boolean.parseBoolean(config.getProperty("mail.enable"))) {
+                        Mail mail = new Mail();
+
+                        // Send email
+                        mail.send("Administrator", "", config.getProperty("mail.administrator"),
+                                user.getEmail(), "Password Retrieval",
+                                "Dear " + user.getFirstName() + "," +
+                                "\n\nYour new password is " + password + ". You received this email because " +
+                                "you have forgotten your password or someone pretending to be you " +
+                                "is trying to log into the system." +
+                                "\n\nYours truly," +
+                                "\nAdministrator");
+                    }
+
+                    forward = Constants.PASSWORD_SUCCESS;
                 } else {
-                    request.setAttribute("error", "1");
-                    return mapping.findForward(FAILURE);
+                    ActionMessages errors = new ActionMessages();
+                    errors.add("question", new ActionMessage("error.question.invalid"));
+                    saveErrors(request, errors);
+
+                    forward = Constants.PASSWORD_REDO;
+                }
+
+                return mapping.findForward(forward);
+            }
+        } else {
+            return mapping.findForward(Constants.PASSWORD_REDO);
+        }
+    }
+
+    /**
+     * Validates the inputs from the user form.
+     * 
+     * @param request   the HTTP Request we are processing
+     * @param form      the ActionForm bean for this request
+     * @return          <code>true</code> if there are no errors in the form; <code>false</code> otherwise
+     */
+    private boolean isValidAccount(HttpServletRequest request, ActionForm form) {
+        ActionMessages errors = new ActionMessages();
+        Validator validator = new Validator();
+        boolean isValid = true;
+
+        PasswordForm passwordForm = (PasswordForm) form;
+        String username = passwordForm.getUsername();
+        String answer = passwordForm.getAnswer();
+
+        if (username == null) {
+            errors.add("username", new ActionMessage(""));
+        } else {
+            if (username.length() < 1) {
+                errors.add("username", new ActionMessage("error.username.required"));
+            } else {
+                if (!validator.isValidUsername(username)) {
+                    errors.add("username", new ActionMessage("error.username.invalid"));
                 }
             }
-        } catch (SQLException sqle)  {
-            sqlMap.endTransaction();
-            throw new SQLException(sqle.getCause());
-        } catch (AddressException ae) {
-            return mapping.findForward(FAILURE);
-        } catch (MessagingException me) {
-            return mapping.findForward(FAILURE);
-        } catch (Exception e){
-            return mapping.findForward(FAILURE);
         }
+
+        if (answer == null) {
+            errors.add("answer", new ActionMessage(""));
+        } else {
+            if (answer.length() < 1) {
+                errors.add("answer", new ActionMessage("error.answer.required"));
+            } else {
+                if (!validator.isValidKeyword(answer)) {
+                    errors.add("answer", new ActionMessage("error.answer.invalid"));
+                }
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            saveErrors(request, errors);
+            isValid = false;
+        }
+
+        return isValid;
     }
 }
