2400b936762e646865f8ab1ebb23c26bc325cf74
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-02-04 12:37:29 +0000

    Fixed formatting of source code. Replaced forwarding instances with those from Constants.

---
+++ src/com/googlecode/openmpis/action/AuthenticateAction.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of OpenMPIS, the Open Source Missing Persons Information System.
+ * Copyright (C) 2008  Rey Vincent Babilonia <rvbabilonia@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.googlecode.openmpis.action;
+
+import com.googlecode.openmpis.util.Constants;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionForward;
+
+/**
+ * The AuthenticateAction class provides the method to check if there exists a session.
+ * 
+ * @author  <a href="mailto:rvbabilonia@gmail.com">Rey Vincent Babilonia</a>
+ */
+public class AuthenticateAction extends Action {
+
+    /**
+     * This is the action called from the Struts framework.
+     * 
+     * @param   mapping     the ActionMapping used to select this instance
+     * @param   form        the optional ActionForm bean for this request
+     * @param   request     the HTTP Request we are processing
+     * @param   response    the HTTP Response we are processing
+     * @return              the forwarding instance
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) {
+        // Check if there exists a session
+        if (request.getSession().getAttribute("currentuser") == null) {
+            return mapping.findForward(Constants.EXPIRED);
+        } else {
+            return mapping.findForward(Constants.AUTHENTICATED);
+        }
+    }
+}
