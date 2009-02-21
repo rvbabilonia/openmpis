243bc007eb2f4f5ddb6d3bef7951a8eba23eb1ca
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-02-21 17:29:40 +0000

    Fixed comments. Fixed regular expressions. Removed isValidPassword method.

--- src/com/googlecode/openmpis/util/Validator.java
+++ src/com/googlecode/openmpis/util/Validator.java
@@ -21,21 +21,21 @@ package com.googlecode.openmpis.util;
  * The Validator class provides methods to sanitize inputs.
  * 
  * @author  <a href="mailto:rvbabilonia@gmail.com">Rey Vincent Babilonia</a>
+ * @version 1.0
  */
 public class Validator {
 
     /**
-     * Sole constructor.
+     * Creates a new validator.
      */
     public Validator() {
-        super();
     }
 
     /**
      * Validates the ID.
      * 
-     * @param   id              the ID
-     * @return          true    if ID is valid, otherwise false
+     * @param id            the ID
+     * @return              <code>true</code> if ID is valid; <code>false</code> otherwise
      */
     public boolean isValidID(String id) {
         return id.matches("[0-9]+");
@@ -44,28 +44,28 @@ public class Validator {
     /**
      * Validates the first name.
      * 
-     * @param   firstName           the first name
-     * @return              true    if the first name is valid, otherwise false
+     * @param firstName     the first name
+     * @return              <code>true</code> if the first name is valid; <code>false</code> otherwise
      */
     public boolean isValidFirstName(String firstName) {
-        return firstName.matches("[A-Z][a-z]+([ '-][a-zA-Z]+)*");
+        return firstName.matches("[A-Z][a-z]+([\\s'-][a-zA-Z]+)*");
     }
 
     /**
      * Validates the last name.
      * 
-     * @param   lastName            the last name
-     * @return              true    if the last name is valid, otherwise false
+     * @param lastName      the last name
+     * @return              <code>true</code> if the last name is valid; <code>false</code> otherwise
      */
     public boolean isValidLastName(String lastName) {
-        return lastName.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*");
+        return lastName.matches("[A-Z][a-z]+|[A-Z][a-z]+([\\s][A-Z][a-z]+)+|([a-z]+[\\s])+[A-Z][a-z]+|[a-z][\'][A-Z][a-z]+|[A-Z][a-z]+[\\-][A-Za-z][a-z]+");
     }
 
     /**
      * Validates the email address of the sender.
      * 
-     * @param   address         the email address of the sender
-     * @return          true    if the email address is valid, otherwise false
+     * @param address       the email address of the sender
+     * @return              <code>true</code> if the email address is valid; <code>false</code> otherwise
      */
     public boolean isValidEmailAddress(String address) {
         return address.matches(".+@.+\\.[a-z]+");
@@ -74,40 +74,30 @@ public class Validator {
     /**
      * Validates the keyword to be searched.
      * 
-     * @param   keyword         the keyword to be searched
-     * @return          true    if the keyword is valid, otherwise false
+     * @param keyword       the keyword to be searched
+     * @return              <code>true</code> if the keyword is valid; <code>false</code> otherwise
      */
     public boolean isValidKeyword(String keyword) {
-        return keyword.matches("[a-zA-Z0-9 ]*");
+        return keyword.matches("[a-zA-Z0-9\\s\\.\\-]*");
     }
 
     /**
      * Validates the username.
      * 
-     * @param   username            the username
-     * @return              true    if the username is valid, otherwise false
+     * @param username      the username
+     * @return              <code>true</code> if the username is valid; <code>false</code> otherwise
      */
     public boolean isValidUsername(String username) {
-        return username.matches("[a-zA-Z0-9]{5,6}");
-    }
-
-    /**
-     * Validates the 32 alphanumeric characters MD5-encrypted password.
-     * 
-     * @param   password            the password
-     * @return              true    if the password is valid, otherwise false
-     */
-    public boolean isValidPassword(String password) {
-        return password.matches(".{32}");
+        return username.matches("[A-Z]{2}[0-9]{4}");
     }
 
     /**
      * Validates the telephone number.
      * 
-     * @param   number              the telephone number
-     * @return              true    if the telephone number is valid, otherwise false
+     * @param number        the telephone number
+     * @return              <code>true</code> if the telephone number is valid; <code>false</code> otherwise
      */
     public boolean isValidNumber(String number) {
-        return number.matches("[a-zA-Z0-9\\Q+-.\\E]+");
+        return number.matches("[0-9\\s\\Q+-.\\E()]+(loc\\.\\s|LOC\\.\\s|EXT\\.\\s|ext\\.\\s)?[0-9]+");
     }
 }
