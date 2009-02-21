f7fcb7011e32f531a5a399dbdbc36eac06230eb0
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-02-21 17:27:57 +0000

    Added new constants. Added version number.

--- trunk/src/com/googlecode/openmpis/util/Constants.java
+++ trunk/src/com/googlecode/openmpis/util/Constants.java
@@ -21,99 +21,140 @@ package com.googlecode.openmpis.util;
  * The Constants class provides constant values.
  * 
  * @author  <a href="mailto:rvbabilonia@gmail.com">Rey Vincent Babilonia</a>
+ * @version 1.0
  */
 public abstract class Constants {
 
     /**
-     * 
+     * The OpenMPIS version
+     */
+    public static final double VERSION = 1.0;
+    /**
+     * The logical name of the forwarding instance for general failure
      */
     public static final String FAILURE = "failure";
     /**
-     * 
+     * The logical name of the forwarding instance for expired session
      */
     public static final String EXPIRED = "expired";
     /**
-     * 
+     * The logical name of the forwarding instance for unauthorized access
      */
     public static final String UNAUTHORIZED = "unauthorized";
     /**
-     * 
+     * The logical name of the forwarding instance for authenticated user
      */
     public static final String AUTHENTICATED = "authenticated";
     /**
-     * 
+     * The logical name of the forwarding instance for successful user listing
      */
-    public static final String LIST_SUCCESS = "list-success";
+    public static final String LIST_USER = "list-user";
     /**
-     * 
+     * The logical name of the forwarding instance for adding new user
      */
     public static final String ADD_USER = "add-user";
     /**
-     * 
+     * The logical name of the forwarding instance for unsuccessful adding of new user
      */
-    public static final String ADD_REDO = "add-redo";
+    public static final String ADD_USER_REDO = "add-user-redo";
     /**
-     * 
+     * The logical name of the forwarding instance for successful adding of new user
      */
-    public static final String ADD_SUCCESS = "add-success";
+    public static final String ADD_USER_SUCCESS = "add-user-success";
     /**
-     * 
+     * The logical name of the forwarding instance for viewing and editing user profile
      */
     public static final String EDIT_USER = "edit-user";
     /**
-     * 
+     * The logical name of the forwarding instance for unsuccessful editing of user profile
      */
-    public static final String EDIT_REDO = "edit-redo";
+    public static final String EDIT_USER_REDO = "edit-user-redo";
     /**
-     * 
+     * The logical name of the forwarding instance for successful editing of user profile
      */
-    public static final String EDIT_SUCCESS = "edit-success";
+    public static final String EDIT_USER_SUCCESS = "edit-user-success";
     /**
-     * 
+     * The logical name of the forwarding instance for deleting a user
      */
     public static final String DELETE_USER = "delete-user";
     /**
-     * 
+     * The logical name of the forwarding instance for deletion confirmation code mismatch
      */
-    public static final String DELETE_REDO = "delete-redo";
+    public static final String DELETE_USER_REDO = "delete-user-redo";
     /**
-     * 
+     * The logical name of the forwarding instance for successful deletion
      */
-    public static final String DELETE_FAILURE = "delete-failure";
+    public static final String DELETE_USER_SUCCESS = "delete-user-success";
     /**
-     * 
-     */
-    public static final String DELETE_SUCCESS = "delete-success";
-    /**
-     * 
+     * The logical name of the forwarding instance for unsuccessful sending of feedback
      */
     public static final String FEEDBACK_REDO = "feedback-redo";
     /**
-     * 
+     * The logical name of the forwarding instance for successful sending of feedback
      */
     public static final String FEEDBACK_SUCCESS = "feedback-success";
     /**
-     * 
+     * The logical name of the forwarding instance for unsuccessful log in
      */
     public static final String LOGIN_REDO = "login-redo";
     /**
-     * 
+     * The logical name of the forwarding instance for successful log in
      */
     public static final String LOGIN_SUCCESS = "login-success";
     /**
-     * 
+     * The logical name of the forwarding instance for log out
      */
     public static final String LOGOUT_SUCCESS = "logout-success";
     /**
-     * 
+     * The logical name of the forwarding instance for unsuccessful password reset
      */
     public static final String PASSWORD_REDO = "password-redo";
     /**
-     * 
+     * The logical name of the forwarding instance for successful password reset
      */
     public static final String PASSWORD_SUCCESS = "password-success";
     /**
-     * 
+     * The logical name of the forwarding instance for successful user count
      */
-    public static final String VIEW_USER = "view-user";
+    public static final String USER_STATISTICS = "user-statistics";
+    /**
+     * The logical name of the forwarding instance for successful case count
+     */
+    public static final String CASE_STATISTICS = "case-statistics";
+    /**
+     * The logical name of the forwarding instance for successful person listing
+     */
+    public static final String LIST_PERSON = "list-person";
+    /**
+     * The logical name of the forwarding instance for viewing a case
+     */
+    public static final String VIEW_PERSON = "view-person";
+    /**
+     * The logical name of the forwarding instance for adding a new person
+     */
+    public static final String ADD_PERSON = "add-person";
+    /**
+     * The logical name of the forwarding instance for unsuccessful adding of a new person
+     */
+    public static final String ADD_PERSON_REDO = "add-person-redo";
+    /**
+     * The logical name of the forwarding instance for successful adding of a new person
+     */
+    public static final String ADD_PERSON_SUCCESS = "add-person-success";
+    /**
+     * The logical name of the forwarding instance for viewing a person's poster
+     */
+    public static final String VIEW_PERSON_POSTER = "view-person-poster";
+    /**
+     * The logical name of the forwarding instance for successful listing of all missing persons
+     */
+    public static final String LIST_PERSON_ALL_MISSING = "list-person-all-missing";
+    /**
+     * The logical name of the forwarding instance for successful listing of all missing persons
+     */
+    public static final String LIST_PERSON_ALL_FOUND = "list-person-all-found";
+    /**
+     * The logical name of the forwarding instance for successful listing of all missing persons
+     */
+    public static final String LIST_PERSON_UNIDENTIFIED = "list-person-unidentified";
 }
