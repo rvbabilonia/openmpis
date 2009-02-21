d52d829438150dd7b6205918d66d9cafa61b081d
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-02-21 17:58:16 +0000

    Fixed comments. Added logger.

--- src/com/googlecode/openmpis/action/LoginAction.java
+++ src/com/googlecode/openmpis/action/LoginAction.java
@@ -17,13 +17,17 @@
  */
 package com.googlecode.openmpis.action;
 
+import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 
 import com.googlecode.openmpis.dto.Log;
 import com.googlecode.openmpis.dto.User;
@@ -35,32 +39,39 @@ import com.googlecode.openmpis.persistence.ibatis.service.UserService;
 import com.googlecode.openmpis.persistence.ibatis.service.impl.LogServiceImpl;
 import com.googlecode.openmpis.persistence.ibatis.service.impl.UserServiceImpl;
 import com.googlecode.openmpis.util.Constants;
-
 import com.googlecode.openmpis.util.Validator;
-import java.text.SimpleDateFormat;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 
 /**
  * The LoginAction class provides the method to authenticate a user.
  * 
  * @author  <a href="mailto:rvbabilonia@gmail.com">Rey Vincent Babilonia</a>
+ * @version 1.0
  */
 public class LoginAction extends Action {
 
     /**
+     * The file logger
+     */
+    private Logger logger = Logger.getLogger(this.getClass());
+
+    /**
      * This is the action called from the Struts framework.
      * 
-     * @param   mapping     the ActionMapping used to select this instance
-     * @param   form        the optional ActionForm bean for this request
-     * @param   request     the HTTP Request we are processing
-     * @param   response    the HTTP Response we are processing
+     * @param mapping       the ActionMapping used to select this instance
+     * @param form          the optional ActionForm bean for this request
+     * @param request       the HTTP Request we are processing
+     * @param response      the HTTP Response we are processing
      * @return              the forwarding instance
      * @throws  java.lang.Exception
      */
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // Checks if there is an existing session thus no need to login
+        if (request.getSession().getAttribute("currentuser") != null) {
+            return mapping.findForward(Constants.LOGIN_SUCCESS);
+        }
+
         LoginForm loginForm = (LoginForm) form;
         UserService userService = new UserServiceImpl(new UserDAOImpl());
         LogService logService = new LogServiceImpl(new LogDAOImpl());
@@ -84,13 +95,14 @@ public class LoginAction extends Action {
                 user.setIpAddress(ipAddress);
 
                 // Set user login event
-                Log log = new Log();
-                log.setLog("User " + user.getUsername() + " logged in from " + ipAddress + ".");
-                log.setDate(date);
+                Log loginLog = new Log();
+                loginLog.setLog("User " + user.getUsername() + " logged in from " + ipAddress + ".");
+                loginLog.setDate(date);
 
                 // Update user's last login date and IP address and insert log
                 userService.updateLogin(user);
-                logService.insertLog(log);
+                logService.insertLog(loginLog);
+                logger.info(loginLog.toString());
 
                 forward = Constants.LOGIN_SUCCESS;
             } else {
@@ -98,11 +110,16 @@ public class LoginAction extends Action {
                 errors.add("login", new ActionMessage("error.login.invalid"));
                 saveErrors(request, errors);
 
+                logger.info("Invalid password for user " + loginForm.getJ_username() +
+                        " from " + request.getRemoteAddr() + ".");
+
                 forward = Constants.LOGIN_REDO;
             }
 
             return mapping.findForward(forward);
         } else {
+            logger.info("Invalid login credentials from " + request.getRemoteAddr() + ".");
+
             return mapping.findForward(Constants.LOGIN_REDO);
         }
     }
@@ -110,9 +127,9 @@ public class LoginAction extends Action {
     /**
      * Validates the inputs from the user form.
      * 
-     * @param request   the HTTP Request we are processing
-     * @param form      the ActionForm bean for this request
-     * @return          <code>true</code> if there are no errors in the form; <code>false</code> otherwise
+     * @param request       the HTTP Request we are processing
+     * @param form          the ActionForm bean for this request
+     * @return              <code>true</code> if there are no errors in the form; <code>false</code> otherwise
      */
     private boolean isValidLogin(HttpServletRequest request, ActionForm form) {
         ActionMessages errors = new ActionMessages();
