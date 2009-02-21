e460a68c6d019ac61c2df73a648abbb9ad9cecbc
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-02-21 17:32:58 +0000

    Added new count methods. Fixed comments.

--- trunk/src/com/googlecode/openmpis/persistence/ibatis/dao/UserDAO.java
+++ trunk/src/com/googlecode/openmpis/persistence/ibatis/dao/UserDAO.java
@@ -18,6 +18,7 @@
 package com.googlecode.openmpis.persistence.ibatis.dao;
 
 import com.googlecode.openmpis.dto.User;
+import com.googlecode.openmpis.util.Pagination;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -33,17 +34,18 @@ public interface UserDAO {
 
     /**
      * Retrieves all users.
-     * 
-     * @return      the list of users
+     *
+     * @param pagination    the pagination context
+     * @return              the list of users
      * @throws java.sql.SQLException
      */
-    public List getAllUsers() throws SQLException;
+    public List<User> getAllUsers(Pagination pagination) throws SQLException;
 
     /**
      * Retrieves a user given his ID.
      * 
-     * @param id    the user ID
-     * @return      the user
+     * @param id            the user ID
+     * @return              the user
      * @throws java.sql.SQLException
      */
     public User getUserById(Integer id) throws SQLException;
@@ -51,8 +53,8 @@ public interface UserDAO {
     /**
      * Retrieves a user given his username.
      * 
-     * @param username  the username
-     * @return          the user
+     * @param username      the username
+     * @return              the user
      * @throws java.sql.SQLException
      */
     public User getUserByUsername(String username) throws SQLException;
@@ -60,8 +62,8 @@ public interface UserDAO {
     /**
      * Inserts a new user.
      * 
-     * @param user  the new user
-     * @return      <code>true</code> if the user was successfully inserted; <code>false</code> otherwise
+     * @param user          the new user
+     * @return              <code>true</code> if the user was successfully inserted; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean insertUser(User user) throws SQLException;
@@ -69,8 +71,8 @@ public interface UserDAO {
     /**
      * Updates an existing user.
      * 
-     * @param user  the existing user
-     * @return      <code>true</code> if the user was successfully updated; <code>false</code> otherwise
+     * @param user          the existing user
+     * @return              <code>true</code> if the user was successfully updated; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean updateUser(User user) throws SQLException;
@@ -78,8 +80,8 @@ public interface UserDAO {
     /**
      * Updates a user's last log in date and IP address.
      * 
-     * @param user  the user who logged in
-     * @return      <code>true</code> if the log in date and IP address were successfully updated; <code>false</code> otherwise
+     * @param user          the user who logged in
+     * @return              <code>true</code> if the log in date and IP address were successfully updated; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean updateLogin(User user) throws SQLException;
@@ -87,8 +89,8 @@ public interface UserDAO {
     /**
      * Resets a user's password.
      * 
-     * @param user  the user who supplied the correct security question and answer
-     * @return      <code>true</code> if the password was successfully reset; <code>false</code> otherwise
+     * @param user          the user who supplied the correct security question and answer
+     * @return              <code>true</code> if the password was successfully reset; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean updatePassword(User user) throws SQLException;
@@ -96,8 +98,8 @@ public interface UserDAO {
     /**
      * Deletes a user with the specified ID.
      * 
-     * @param id    the ID of the user to be deleted
-     * @return      <code>true</code> if the user was successfully deleted; <code>false</code> otherwise
+     * @param id            the ID of the user to be deleted
+     * @return              <code>true</code> if the user was successfully deleted; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean deleteUser(Integer id) throws SQLException;
@@ -105,8 +107,8 @@ public interface UserDAO {
     /**
      * Checks if a username is unique.
      * 
-     * @param user  the existing user
-     * @return      <code>true</code> if the username is unique; <code>false</code> otherwise
+     * @param user          the existing user
+     * @return              <code>true</code> if the username is unique; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean isUniqueUsername(User user) throws SQLException;
@@ -114,9 +116,57 @@ public interface UserDAO {
     /**
      * Checks if an email address is unique.
      * 
-     * @param user  the existing user
-     * @return      <code>true</code> if the email address is unique; <code>false</code> otherwise
+     * @param user          the existing user
+     * @return              <code>true</code> if the email address is unique; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     public boolean isUniqueEmail(User user) throws SQLException;
+
+    /**
+     * Returns the total number of users.
+     *
+     * @return              the total number of users
+     * @throws java.sql.SQLException
+     */
+    public int countAllUsers() throws SQLException;
+
+    /**
+     * Returns the total number of administrators.
+     *
+     * @return              the total number of administrators
+     * @throws java.sql.SQLException
+     */
+    public int countAdministrators() throws SQLException;
+
+    /**
+     * Returns the total number of encoders.
+     *
+     * @return              the total number of encoders
+     * @throws java.sql.SQLException
+     */
+    public int countEncoders() throws SQLException;
+
+    /**
+     * Returns the total number of investigators.
+     *
+     * @return              the total number of investigators
+     * @throws java.sql.SQLException
+     */
+    public int countInvestigators() throws SQLException;
+
+    /**
+     * Returns the total number of active users.
+     *
+     * @return              the total number of active users
+     * @throws java.sql.SQLException
+     */
+    public int countActiveUsers() throws SQLException;
+
+    /**
+     * Returns the total number of suspended users.
+     *
+     * @return              the total number of suspended users
+     * @throws java.sql.SQLException
+     */
+    public int countSuspendedUsers() throws SQLException;
 }
--- trunk/src/com/googlecode/openmpis/persistence/ibatis/service/impl/UserServiceImpl.java
+++ trunk/src/com/googlecode/openmpis/persistence/ibatis/service/impl/UserServiceImpl.java
@@ -20,6 +20,7 @@ package com.googlecode.openmpis.persistence.ibatis.service.impl;
 import com.googlecode.openmpis.dto.User;
 import com.googlecode.openmpis.persistence.ibatis.dao.UserDAO;
 import com.googlecode.openmpis.persistence.ibatis.service.UserService;
+import com.googlecode.openmpis.util.Pagination;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -35,14 +36,14 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     /**
-     * The UserDAO
+     * The user DAO
      */
     private UserDAO userDAO = null;
-    
+
     /**
-     * Sole constructor.
+     * Creates a new user service implementation.
      * 
-     * @param userDAO   the user DAO
+     * @param userDAO       the user DAO
      */
     public UserServiceImpl(UserDAO userDAO) {
         this.userDAO = userDAO;
@@ -51,8 +52,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Deletes a user with the specified ID.
      * 
-     * @param id    the ID of the user to be deleted
-     * @return      <code>true</code> if the user was successfully deleted; <code>false</code> otherwise
+     * @param id            the ID of the user to be deleted
+     * @return              <code>true</code> if the user was successfully deleted; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
@@ -62,20 +63,21 @@ public class UserServiceImpl implements UserService {
 
     /**
      * Retrieves all users.
-     * 
-     * @return      the list of users
+     *
+     * @param pagination    the pagination context
+     * @return              the list of users
      * @throws java.sql.SQLException
      */
     @Override
-    public List<User> getAllUsers() throws SQLException {
-        return userDAO.getAllUsers();
+    public List<User> getAllUsers(Pagination pagination) throws SQLException {
+        return userDAO.getAllUsers(pagination);
     }
 
     /**
      * Retrieves a user given his ID.
      * 
-     * @param id    the user ID
-     * @return      the user
+     * @param id            the user ID
+     * @return              the user
      * @throws java.sql.SQLException
      */
     @Override
@@ -86,8 +88,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Retrieves a user given his username.
      * 
-     * @param username  the username
-     * @return          the user
+     * @param username      the username
+     * @return              the user
      * @throws java.sql.SQLException
      */
     @Override
@@ -98,8 +100,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Inserts a new user.
      * 
-     * @param user  the new user
-     * @return      <code>true</code> if the user was successfully inserted; <code>false</code> otherwise
+     * @param user          the new user
+     * @return              <code>true</code> if the user was successfully inserted; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
@@ -110,8 +112,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Checks if an email address is unique.
      * 
-     * @param user  the existing user
-     * @return      <code>true</code> if the email address is unique; <code>false</code> otherwise
+     * @param user          the existing user
+     * @return              <code>true</code> if the email address is unique; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
@@ -122,8 +124,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Checks if a username is unique.
      * 
-     * @param user  the existing user
-     * @return      <code>true</code> if the username is unique; <code>false</code> otherwise
+     * @param user          the existing user
+     * @return              <code>true</code> if the username is unique; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
@@ -134,8 +136,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Updates a user's last log in date and IP address.
      * 
-     * @param user  the user who logged in
-     * @return      <code>true</code> if the log in date and IP address were successfully updated; <code>false</code> otherwise
+     * @param user          the user who logged in
+     * @return              <code>true</code> if the log in date and IP address were successfully updated; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
@@ -146,8 +148,8 @@ public class UserServiceImpl implements UserService {
     /**
      * Resets a user's password.
      * 
-     * @param user  the user who supplied the correct security question and answer
-     * @return      <code>true</code> if the password was successfully reset; <code>false</code> otherwise
+     * @param user          the user who supplied the correct security question and answer
+     * @return              <code>true</code> if the password was successfully reset; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
@@ -158,12 +160,78 @@ public class UserServiceImpl implements UserService {
     /**
      * Updates an existing user.
      * 
-     * @param user  the existing user
-     * @return      <code>true</code> if the user was successfully updated; <code>false</code> otherwise
+     * @param user          the existing user
+     * @return              <code>true</code> if the user was successfully updated; <code>false</code> otherwise
      * @throws java.sql.SQLException
      */
     @Override
     public boolean updateUser(User user) throws SQLException {
         return userDAO.updateUser(user);
+    }
+
+    /**
+     * Returns the total number of users.
+     *
+     * @return              the total number of users
+     * @throws java.sql.SQLException
+     */
+    @Override
+    public int countAllUsers() throws SQLException {
+        return userDAO.countAllUsers();
+    }
+
+    /**
+     * Returns the total number of administrators.
+     *
+     * @return              the total number of administrators
+     * @throws java.sql.SQLException
+     */
+    @Override
+    public int countAdministrators() throws SQLException {
+        return userDAO.countAdministrators();
+    }
+
+    /**
+     * Returns the total number of encoders.
+     *
+     * @return              the total number of encoders
+     * @throws java.sql.SQLException
+     */
+    @Override
+    public int countEncoders() throws SQLException {
+        return userDAO.countEncoders();
+    }
+
+    /**
+     * Returns the total number of investigators.
+     *
+     * @return              the total number of investigators
+     * @throws java.sql.SQLException
+     */
+    @Override
+    public int countInvestigators() throws SQLException {
+        return userDAO.countInvestigators();
+    }
+
+    /**
+     * Returns the total number of active users.
+     *
+     * @return              the total number of active users
+     * @throws java.sql.SQLException
+     */
+    @Override
+    public int countActiveUsers() throws SQLException {
+        return userDAO.countActiveUsers();
+    }
+
+    /**
+     * Returns the total number of suspended users.
+     *
+     * @return              the total number of suspended users
+     * @throws java.sql.SQLException
+     */
+    @Override
+    public int countSuspendedUsers() throws SQLException {
+        return userDAO.countSuspendedUsers();
     }
 }
