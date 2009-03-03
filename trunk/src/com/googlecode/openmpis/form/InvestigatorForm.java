2138f3d993c550bccf82377834f11bf04d5751a1
rvbabilonia <rvbabilonia@be8d52ce-ce47-0410-8d56-55b35f6ce4b8>
2009-03-03 12:08:09 +0000

    Added investigator action form.

---
+++ trunk/src/com/googlecode/openmpis/form/InvestigatorForm.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of OpenMPIS, the Open Source Missing Persons Information System.
+ * Copyright (C) 2008  Rey Vincent Babilonia <rvbabilonia@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.googlecode.openmpis.form;
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ * The InvestigatorForm class is used to assign an investigator.
+ * 
+ * @author  <a href="mailto:rvbabilonia@gmail.com">Rey Vincent Babilonia</a>
+ * @version 1.0
+ */
+public class InvestigatorForm extends ActionForm {
+
+    /**
+     * The investigator ID
+     */
+    private int id;
+
+    /**
+     * Gets the ID of the investigator.
+     *
+     * @return               the ID of the investigator
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the ID of the investigator.
+     *
+     * @param id            the ID of the investigator
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns a String representation of this data transfer object.
+     *
+     * @return              the String representation of this data transfer object
+     */
+    @Override
+    public String toString() {
+        String content = "";
+        content += "\nID: " + id;
+        return content;
+    }
+}
